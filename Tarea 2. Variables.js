4529423b514ab575649f6c9c07139c0de9189482
Daniel Pereira <92984764+DanielDEVp@users.noreply.github.com>
2025-01-15 20:50:04 +0100

    Added .js file

---
+++ Tarea 2. Variables.js
@@ -0,0 +1,22 @@
+// Definir variables del  usuario
+let nombre = "Juan";
+let edad = 30;
+let ciudad = "Madrid";
+let profesion = "Ingeniero";
+
+// Imprimir las variables
+console.log("Perfil de Usuario:");
+console.log("Nombre: " + nombre);
+console.log("Edad: " + edad);
+console.log("Ciudad: " + ciudad);
+console.log("Profesión: " + profesion);
+
+// Cambiar una de las variables
+ciudad = "Barcelona";
+
+// Imprimir las variables otra vez
+console.log("\nPerfil de Usuario Actualizado:");
+console.log("Nombre: " + nombre);
+console.log("Edad: " + edad);
+console.log("Ciudad: " + ciudad);
+console.log("Profesión: " + profesion);
